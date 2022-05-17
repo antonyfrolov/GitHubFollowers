34fb4b55c09671f891a8e198e0226989c7019a0c
Anton Frolov <frolan@ya.ru>
2022-05-17 18:07:38 +0300

    SerachVC was added

---
+++ GitHubFollowers/SerachVC.swift
@@ -0,0 +1,17 @@
+//
+//  SerachVCViewController.swift
+//  GitHubFollowers
+//
+//  Created by antonfrolov on 17.05.2022.
+//
+
+import UIKit
+
+class SerachVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
